d040f54f01daee6f1a4268ca2cb07307b4097997
gohlinka2 <gohlinka2@gmail.com>
2024-11-22 17:40:55 +0100

    Update design values

--- coreUi/src/main/java/cz/frantisekhlinka/amiright/coreui/theme/Color.kt
+++ coreUi/src/main/java/cz/frantisekhlinka/amiright/coreui/theme/Color.kt
@@ -2,10 +2,9 @@ package cz.frantisekhlinka.amiright.coreui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val NegativeRedHint = Color(0xFFD49D9D)
+val PositiveGreenHint = Color(0xFF9DD4B3)
+val NegativeRed = Color(0xFFE17676)
+val PositiveGreen = Color(0xFF97DB9B)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val HintText = Color(0xFF949494)
