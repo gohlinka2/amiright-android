1dfb865d3bfcdaf750c41f26e7c6bb216cc0d5ea
gohlinka2 <gohlinka2@gmail.com>
2024-11-22 12:47:03 +0100

    Bind firebase deps

--- app/src/main/java/cz/frantisekhlinka/amiright/di/koin.kt
+++ app/src/main/java/cz/frantisekhlinka/amiright/di/koin.kt
@@ -1,7 +1,12 @@
 package cz.frantisekhlinka.amiright.di
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.functions.FirebaseFunctions
 import org.koin.dsl.module
 
 val appModule = module {
-
+    single { FirebaseAuth.getInstance() }
+    single { FirebaseFirestore.getInstance() }
+    single { FirebaseFunctions.getInstance() }
 }
