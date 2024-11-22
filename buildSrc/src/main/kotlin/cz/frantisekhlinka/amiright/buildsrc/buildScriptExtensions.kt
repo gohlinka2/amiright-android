7aff50d18ba061471fa837628e3a3660a1b5f09b
gohlinka2 <gohlinka2@gmail.com>
2024-11-22 13:57:20 +0100

    Add dependencies to the feature module

--- buildSrc/src/main/kotlin/cz/frantisekhlinka/amiright/buildsrc/buildScriptExtensions.kt
+++ buildSrc/src/main/kotlin/cz/frantisekhlinka/amiright/buildsrc/buildScriptExtensions.kt
@@ -3,29 +3,21 @@ import org.gradle.api.artifacts.Dependency
 import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.gradle.api.plugins.PluginContainer
 import org.gradle.kotlin.dsl.DependencyHandlerScope
-import org.gradle.kotlin.dsl.add
 
 fun PluginContainer.kotlin(module: String): Plugin<Any> = apply("org.jetbrains.kotlin.$module")
 
-/**
- * Adds a dependency to the 'implementation' configuration.
- *
- * @param dependencyNotation notation for the dependency to be added.
- * @return The dependency.
- *
- * @see [DependencyHandler.add]
- */
 fun DependencyHandlerScope.implementation(dependencyNotation: Any): Dependency? =
     add("implementation", dependencyNotation)
 
-/**
- * Adds a dependency to the 'api' configuration.
- *
- * @param dependencyNotation notation for the dependency to be added.
- * @return The dependency.
- *
- * @see [DependencyHandler.add]
- */
+fun DependencyHandlerScope.testImplementation(dependencyNotation: Any): Dependency? =
+    add("testImplementation", dependencyNotation)
+
+fun DependencyHandlerScope.androidTestImplementation(dependencyNotation: Any): Dependency? =
+    add("androidTestImplementation", dependencyNotation)
+
+fun DependencyHandlerScope.debugImplementation(dependencyNotation: Any): Dependency? =
+    add("debugImplementation", dependencyNotation)
+
 fun DependencyHandler.api(dependencyNotation: Any): Dependency? =
     add("api", dependencyNotation)
 
--- buildSrc/src/main/kotlin/cz/frantisekhlinka/amiright/buildsrc/plugins/AppFeatureModule.kt
+++ buildSrc/src/main/kotlin/cz/frantisekhlinka/amiright/buildsrc/plugins/AppFeatureModule.kt
@@ -1,7 +1,13 @@
 package cz.frantisekhlinka.amiright.buildsrc.plugins
 
+import compose
+import implementation
+import testImplementation
+import androidTestImplementation
+import debugImplementation
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.DependencyHandlerScope
+import org.gradle.kotlin.dsl.project
 
 /**
  * A plugin for a feature module that contains UI.
@@ -10,7 +16,29 @@ open class AppFeatureModule : AppCoreAndroidPlugin() {
     override fun configureDependencies(scope: DependencyHandlerScope, target: Project) {
         super.configureDependencies(scope, target)
         scope.run {
-            // any libraries that should be included in all feature modules here
+            implementation(project(Modules.coreBack))
+
+            implementation(Libraries.androidCoreKtx)
+            implementation(Libraries.lifecycleKtx)
+            implementation(Libraries.activityCompose)
+            implementation(Libraries.koinAndroid)
+
+            compose {
+                implementation(Libraries.Compose.compose)
+                implementation(Libraries.Compose.graphics)
+                implementation(Libraries.Compose.toolingPreview)
+                implementation(Libraries.Compose.material)
+            }
+
+            testImplementation(Libraries.junit)
+
+            androidTestImplementation(Libraries.androidXJunit)
+            androidTestImplementation(Libraries.espresso)
+            androidTestImplementation(platform(Libraries.Compose.bom))
+            androidTestImplementation(Libraries.Compose.testJunit)
+
+            debugImplementation(Libraries.Compose.tooling)
+            debugImplementation(Libraries.Compose.testManifest)
         }
     }
 }
