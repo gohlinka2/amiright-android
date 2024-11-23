642c2761253779a8470e77603fd4e8edcdd73c16
gohlinka2 <gohlinka2@gmail.com>
2024-11-23 22:37:05 +0100

    Add drawingModifiers.kt

---
+++ coreUi/src/main/java/cz/frantisekhlinka/amiright/coreui/modifiers/drawingModifiers.kt
@@ -0,0 +1,42 @@
+package cz.frantisekhlinka.amiright.coreui.modifiers
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+
+/**
+ * Modifier that draws a dashed border around the layout.
+ * @param strokeWidth The width of the border.
+ * @param color The color of the border.
+ * @param cornerRadiusDp The corner radius of the border.
+ */
+fun Modifier.dashedBorder(strokeWidth: Dp, color: Color, cornerRadiusDp: Dp) = composed(
+    factory = {
+        val density = LocalDensity.current
+        val strokeWidthPx = density.run { strokeWidth.toPx() }
+        val cornerRadiusPx = density.run { cornerRadiusDp.toPx() }
+
+        this.then(
+            Modifier.drawWithCache {
+                onDrawBehind {
+                    val stroke = Stroke(
+                        width = strokeWidthPx,
+                        pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+                    )
+
+                    drawRoundRect(
+                        color = color,
+                        style = stroke,
+                        cornerRadius = CornerRadius(cornerRadiusPx)
+                    )
+                }
+            }
+        )
+    }
+)
