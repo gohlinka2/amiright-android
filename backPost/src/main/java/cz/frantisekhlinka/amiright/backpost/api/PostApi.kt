8dadedf484879ed6f7a0cb49769e6cfb15735a83
gohlinka2 <gohlinka2@gmail.com>
2024-11-24 15:08:02 +0100

    Add base post api

--- backPost/src/main/java/cz/frantisekhlinka/amiright/backpost/api/PostApi.kt
+++ backPost/src/main/java/cz/frantisekhlinka/amiright/backpost/api/PostApi.kt
@@ -1,15 +1,35 @@
 package cz.frantisekhlinka.amiright.backpost.api
 
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import cz.frantisekhlinka.amiright.coreback.communication.DocumentSnapshotWrapper
+import cz.frantisekhlinka.amiright.coreback.communication.parse
 import cz.frantisekhlinka.amiright.coreback.communication.toModelFlow
 import cz.frantisekhlinka.amiright.coredata.Post
 import cz.frantisekhlinka.amiright.coredata.constants.FirebaseKeys
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.tasks.await
 
 internal class PostApi(
     private val firebaseFirestore: FirebaseFirestore
 ) {
+
+    /**
+     * Gets the latest post that was updated after the given [lastPostTimestamp], or the latest post if
+     * [lastPostTimestamp] is null.
+     *
+     * Returns null if there are no posts.
+     */
+    suspend fun getLatestPostAfter(lastPostTimestamp: Long?): Post? {
+        val baseQuery = firebaseFirestore.collection(FirebaseKeys.POSTS)
+            .orderBy(FirebaseKeys.Post.UPDATED_AT, Query.Direction.DESCENDING)
+            .limit(1)
+        val query = when {
+            lastPostTimestamp != null -> baseQuery.whereLessThan(FirebaseKeys.Post.UPDATED_AT, lastPostTimestamp)
+            else -> baseQuery
+        }
+        return query.get().await().documents.firstNotNullOfOrNull { it.parse { asPost() } }
+    }
 
     /**
      * Gets a flow of updates of a post with the given [postId].
