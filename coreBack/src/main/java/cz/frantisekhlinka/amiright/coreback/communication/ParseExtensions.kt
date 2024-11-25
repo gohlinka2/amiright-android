37c22c714cde900cff337bca38f5bc5b1ccd93a2
gohlinka2 <gohlinka2@gmail.com>
2024-11-25 13:09:53 +0100

    Add firebase extension

--- coreBack/src/main/java/cz/frantisekhlinka/amiright/coreback/communication/ParseExtensions.kt
+++ coreBack/src/main/java/cz/frantisekhlinka/amiright/coreback/communication/ParseExtensions.kt
@@ -3,6 +3,7 @@ package cz.frantisekhlinka.amiright.coreback.communication
 import android.util.Log
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.snapshots
 import cz.frantisekhlinka.amiright.coreback.exceptions.FirestoreParseException
 import kotlinx.coroutines.flow.Flow
@@ -14,11 +15,25 @@ import kotlinx.coroutines.flow.map
  * The listener will get removed if a firestore exception is thrown or if the parent coroutine that
  * applied the terminal operator is cancelled.
  *
- * Parses the document into a [T] using the [transform] function. See [DocumentSnapshot.parse] for more details.
+ * Parses the document into [T] using the [transform] function. See [DocumentSnapshot.parse] for more details.
  */
 fun <T> DocumentReference.toModelFlow(transform: DocumentSnapshotWrapper.() -> T): Flow<T?> {
     return snapshots().map {
         it.parse { transform() }
+    }
+}
+
+/**
+ * Converts this Firestore query into a flow of updates of that query's documents.
+ * A snapshot listener will be attached once a terminal operator (such as collect) is applied to the flow.
+ * The listener will get removed if a firestore exception is thrown or if the parent coroutine that
+ * applied the terminal operator is cancelled.
+ *
+ * Parses the documents into [T] using the [transform] function. See [DocumentSnapshot.parse] for more details.
+ */
+fun <T> Query.toModelFlow(transform: DocumentSnapshotWrapper.() -> T): Flow<List<T>> {
+    return snapshots().map { snap ->
+        snap.documents.mapNotNull { doc -> doc.parse { transform() } }
     }
 }
 
